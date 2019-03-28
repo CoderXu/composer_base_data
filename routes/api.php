39fd6f522ad90e57a085e9852ea35b27b7e203f8
xuwenqiang <xuwenqiang@heqi-inc.com>
2019-03-28 17:48:32 +0800

    init

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,34 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(\App\Http\Middleware\TokenValidate::class)
     ->prefix('manager')
     ->group(function () {
+        // 维修项目分类
+        Route::prefix('item_category')
+            ->group(function () {
+                Route::/*middleware(['permission:item_category_create'])->*/
+                post('create', 'ItemCategoryController@create')
+                    ->name('创建项目分类');
+                Route::/*middleware(['permission:item_category_update'])->*/
+                post('update', 'ItemCategoryController@update')
+                    ->name('更新项目分类');
+                Route::/*middleware(['permission:item_category_query'])->*/
+                get('query/{id}', 'ItemCategoryController@query')
+                    ->name('根据ID查询项目分类');
+                Route::/*middleware(['permission:item_category_all'])->*/
+                get('all', 'ItemCategoryController@all')
+                    ->name('查询所有项目分类');
+                Route::/*middleware(['permission:item_category_search'])->*/
+                get('search', 'ItemCategoryController@search')
+                    ->name('搜索项目分类');
+                Route::/*middleware(['permission:item_category_all_by_pid'])->*/
+                get('all/{pid}', 'ItemCategoryController@allByPid')
+                    ->name('根据PID查询所有子类');
+                Route::/*middleware(['permission:item_category_delete'])->*/
+                get('delete/{id}', 'ItemCategoryController@delete')
+                    ->name('删除项目分类');
+                Route::/*middleware(['permission:item_category_move'])->*/
+                get('move/{newPid}/{id}', 'ItemCategoryController@move')
+                    ->name('移动项目分类');
+            });
         //  维修项目管理
         Route::prefix('item')
             ->group(function () {
---
+++ src/Controller/ItemCategoryController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\DB\Mysql\ItemCategory;
+
+class ItemCategoryController extends Controller
+{
+    protected $mActionTitle = '项目分类';
+    protected $mIsInfiniteCategory = true;
+    protected $mInfiniteCategoryParentName = 'pid';
+    protected $mIsInfiniteCategoryLevel = true;
+
+    public function __construct(ItemCategory $itemCategory)
+    {
+        $this->mModel = $itemCategory;
+        return parent::__construct();
+    }
+
+    public function initPutValidation()
+    {
+        $this->mValidation = [
+            'pid' => [
+                'required',
+                'integer',
+                'min:0',
+                'max:4294967295'
+            ],
+            'name' => [
+                'required',
+                'string',
+                'min:1',
+                'max:191',
+            ]
+        ];
+
+        $this->mRequestParamKeys = [
+            'name'
+        ];
+        if ($this->mIsUpdateAction) {
+            unset($this->mValidation[$this->mInfiniteCategoryParentName]);
+        } else {
+            $this->mRequestParamKeys[] = 'pid';
+        }
+
+        $this->mPutActionParamKeys = $this->mRequestParamKeys;
+        $this->mPutActionParamKeys[] = 'pid_path';
+        $this->mPutActionParamKeys[] = 'level';
+        $this->mUniqueEloquentFunc = function ($params) {
+            if ($this->mIsUpdateAction) {
+                $model = $this->mModel->find($params[$this->mModelPK]);
+                if (!$model) $this->throwMyException($this->mActionTitle . '不存在');
+                $params[$this->mInfiniteCategoryLevelName] = $model[$this->mInfiniteCategoryLevelName];
+            }
+
+            return $this->mUniqueEloquent
+                ->where($this->mInfiniteCategoryLevelName, $params[$this->mInfiniteCategoryLevelName])
+                ->where('name', $params['name']);
+        };
+        return parent::initPutValidation();
+    }
+
+    public function allByPid($pid)
+    {
+        return $this->mModel
+            ->where('pid', $pid)
+            ->get();
+    }
+
+    public function search()
+    {
+        $this->mValidation = [
+            'pid' => [
+                'integer',
+                'min:0',
+                'max:4294967295'
+            ],
+            'name' => [
+                'nullable',
+                'string',
+                'min:1',
+                'max:191'
+            ]
+        ];
+        $requestParams = $this->mRequest->only([
+            'pid'
+            , 'name'
+        ]);
+        if (count($requestParams) > 0) {
+            $this->mValidator($requestParams);
+        }
+        foreach ($requestParams as $key => $value) {
+            if (in_array($key, ['name'])) {
+                $this->mModel = $this->mModel
+                    ->where('name'
+                        , 'like'
+                        , '%' . $requestParams['name'] . '%');
+            } else {
+                $this->mModel = $this->mModel
+                    ->where($key, $value);
+            }
+        }
+        return $this->mModel
+            ->orderByDesc('updated_at')
+            ->paginate();
+    }
+}
