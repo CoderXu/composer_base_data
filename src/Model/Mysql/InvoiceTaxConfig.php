cd73726c1316fe2ea73400082d0429bd94ee9bb1
xuwenqiang <xuwenqiang@heqi-inc.com>
2019-04-11 09:55:42 +0800

    修正发票税配置model未更新

--- src/Model/Mysql/InvoiceTaxConfig.php
+++ src/Model/Mysql/InvoiceTaxConfig.php
@@ -8,6 +8,18 @@ class InvoiceTaxConfig extends SAASModel
 {
     protected $table = 'invoice_tax_config';
     protected $guarded = [];
+    public static $TAX_COMPUTE_MODE_FIXED = 1;
+    public static $TAX_COMPUTE_MODE_PERCENT = 2;
+    public static $TAX_COMPUTE_MODE_DIVISION = 3;
+    private $mAmount = 0;
+
+    public function queryById($id)
+    {
+        $result = $this->find($id);
+        if (!$result)
+            $this->throwMyException('发票税配置不存在');
+        return $result;
+    }
 
     public function setValues($params)
     {
@@ -19,20 +31,23 @@ class InvoiceTaxConfig extends SAASModel
 
     /**
      * 根据金额计算税金
-     * @param $amount
+     * @param bool $isFixedIfReturn 主要用于固定税金计算时，是否返回固定税金
      * @return float
      */
-    public function computeTax($amount)
+    public function computeTax($isFixedIfReturn = true)
     {
+        $amount = $this->getAmount();
+        $originAmount = $amount;
+        if ($amount < 0) $amount = 0 - $amount;
         $taxAmount = 0;
         switch ($this->attributes['tax_compute_mode']) {
-            case 1:
-                $taxAmount = $this->computeModeFixed($amount);
+            case self::$TAX_COMPUTE_MODE_FIXED:
+                $taxAmount = $this->computeModeFixed($amount, $isFixedIfReturn);
                 break;
-            case 2:
+            case self::$TAX_COMPUTE_MODE_PERCENT:
                 $taxAmount = $this->computeModePercent($amount);
                 break;
-            case 3:
+            case self::$TAX_COMPUTE_MODE_DIVISION:
                 $taxAmount = $this->computeModeDivision($amount);
                 break;
             case 0:
@@ -41,49 +56,67 @@ class InvoiceTaxConfig extends SAASModel
         // 不允许为负数
         if (bcsub($amount, $taxAmount, 8) <= 0)
             $this->throwMyException('税金不能大于或等于金额');
+        if ($originAmount < 0) $taxAmount = 0 - $taxAmount;
         return $taxAmount;
     }
 
     /**
      * 固定金额方式计算税金
-     * @param $amount
+     * @param bool $isFixedIfReturn
      * @return float
      */
-    public function computeModeFixed($amount)
+    public function computeModeFixed($isFixedIfReturn = false)
     {
-        if ($amount > $this->attributes['value'])
-            return $this->attributes['value'];
-        return $amount;
+        if ($isFixedIfReturn) return 0;
+        //if ($amount > $this->attributes['value'])
+        return $this->attributes['value'];
+        //return $amount;
     }
 
     /**
      * 价格百分比(不含税)方式计算税金
-     * @param $amount
      * @return float
      */
-    public function computeModePercent($amount)
+    public function computeModePercent()
     {
         return AmountUtil::mul(
-            $amount
+            $this->getAmount()
             , AmountUtil::div($this->attributes['value'], 100)
         );
     }
 
     /**
      * 价格百分比(含税)方式计算税金
-     * @param $amount
      * @return float
      */
-    public function computeModeDivision($amount)
+    public function computeModeDivision()
     {
         $rate = AmountUtil::add(
             1
             , AmountUtil::div($this->attributes['value'], 100)
         );
         return AmountUtil::sub(
-            $amount
-            , AmountUtil::div($amount, $rate)
+            $this->getAmount()
+            , AmountUtil::div($this->getAmount(), $rate)
         );
+    }
+
+    /**
+     *
+     */
+    public function getAmount()
+    {
+        return $this->mAmount;
+    }
+
+    /**
+     * @param $mAmount
+     * @return InvoiceTaxConfig
+     */
+    public function setAmount($mAmount)
+    {
+        $this->mAmount = $mAmount;
+        return $this;
     }
 
     public function invoiceMode()
